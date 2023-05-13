617cbe2fcbdfcbbe4fdba8df2ae6497971dc6b89
Cloud-Xero <hiropon201511@gmail.com>
2023-05-13 16:41:37 +0900

    refactor: sort & add semi

--- next-mantine-tailwind/pages/_app.tsx
+++ next-mantine-tailwind/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/globals.css";
+
+import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
